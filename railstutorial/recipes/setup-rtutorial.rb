5235a252512262436bb834baf75a87da8395d8ef
nicklebee <nicholasberry.13@gmail.com>
2016-04-09 13:52:10 -0400

    git clone repo

--- railstutorial/recipes/setup-rtutorial.rb
+++ railstutorial/recipes/setup-rtutorial.rb
@@ -1,15 +1,15 @@
+rtutorial = search("aws_opsworks_app", "shortname:railstutorial").first
+
 Chef::Log.info('Setting up railstutorial server')
 
 execute "apt-get update and upgrade" do
   command "apt-get update && apt-get upgrade"
 end
 
-bash 'Add ruby/rails' do
-  user 'root'
-  cwd '/tmp'
-  code <<-EOH
-      curl -sSL https://get.rvm.io | bash -s stable --ruby
-    EOH
+git "/data/railstutorial" do
+  repo rtutorial["app_source"]["url"]
+  revision rtutorial["app_source"]["revision"]
+  action :sync
 end
 
 package 'nginx' do
