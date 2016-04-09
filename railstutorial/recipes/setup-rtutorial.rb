5753ae16773d645e5ef57d96b2b9de144107bc75
nicklebee <nicholasberry.13@gmail.com>
2016-04-09 14:52:51 -0400

    refactor setup for git

--- railstutorial/recipes/setup-rtutorial.rb
+++ railstutorial/recipes/setup-rtutorial.rb
@@ -6,9 +6,34 @@ execute "apt-get update and upgrade" do
   command "apt-get update && apt-get upgrade"
 end
 
+directory '/data/railstutorial' do
+  recursive :true
+  action :delete
+end
+
+directory '/data/railstutorial' do
+  recursive :true
+  action :create
+end
+
+template "/tmp/id_deploy" do
+  variables( :sshkey => rtutorial["app_source"]["ssh_key"])
+  mode 0600
+  owner "root"
+  group "root"
+  source "sshkey.erb"
+end
+
+cookbook_file "/tmp/wrap-ssh4git.sh" do
+  source "wrap-ssh4git.sh"
+  owner "ubuntu"
+  mode "0755"
+end
+
 git "/data/railstutorial" do
   repo rtutorial["app_source"]["url"]
   revision rtutorial["app_source"]["revision"]
+  ssh_wrapper "/tmp/wrap-ssh4git.sh"
   action :sync
   user "root"
 end
